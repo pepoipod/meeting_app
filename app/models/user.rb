350cb9df4c9f1090ef747364ff99b38f4ae64394
pepoipod <daikinecmouth@hotmail.co.jp>
2015-06-03 15:30:48 +0900

    make user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_many :topic
+
   has_many :users_meeting
   has_many :meeting, :through => :users_meeting
   # Include default devise modules. Others available are:
